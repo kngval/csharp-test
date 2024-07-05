3116581a5759d7b65d4b3688a362579dedadf73a
kngval <bkvalteocs@gmail.com>
2024-07-05 23:14:16 +0800

    access modifiers test

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+
+
+// Your code goes here
+
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within the day");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your Subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine("boop");
+}
+Console.WriteLine(daysUntilExpiration);
---
+++ Test.cs
@@ -0,0 +1,39 @@
+
+// FUNCTIONS || METHOD TEMPLATE
+// <access-modifier> <optional-modifiers> <return-type> <method-name>(<parameters>)
+// {
+//     // Method body
+//     // Statements
+//     // Optionally, return statement
+// }
+
+/* 
+    Access Modifiers 
+        -public
+        -private
+        -protected
+        -internal
+        -protected internal
+        -private protected
+
+    
+    Optional Modifiers
+        -static
+        -abstract
+        -sealed
+        -virtual
+        -override
+        -readonly
+        -const
+        -async/await
+
+*/
+
+
+
+public class Test {
+    public static void test(){
+        Console.WriteLine("Hello test");
+    }
+}
+
